92133be99917212f1e6810a30c0034a0d07a7aae
Lisa <dalex4740@gmail.com>
2024-09-08 12:12:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// const storeName = 'My awesome store'
+
+// const storeDescription = {
+// 	budget: 10000,
+// 	employees: ['Alex', 'John', 'Sam'],
+// 	products: {
+// 		soap: 20,
+		// water: +'5',
+// 	},
+// 	open: true,
+// }
+GitHowTo
+
+    // const answers = 'toys';
+    // answers[0] = prompt ('Как ваше имя?','');
+    // answers[1] = prompt ('Как ваше фамилия?','');
+    // answers[2] = prompt ('Скильки вам роки?','');
+    // console.log(`http://someur.com/${snswer}/5`)
+    
+    // let incr = 10,
+    //     decr = 10;
+    // incr++;
+    // decr--;
+    const lol = false;
+        doc = true;
+    console.log(lol || doc);
+
+
+
